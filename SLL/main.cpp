8f14188856d097d786d5b4269756ed6ec74f4b76
aditya@msi <aditya2352k00@gmail.com>
2021-10-24 18:10:07 +0530

    Modify to SLL and Fix Segmentation fault

--- SLL/main.cpp
+++ SLL/main.cpp
@@ -7,13 +7,9 @@ class Node {
     private:
         T content;
         Node<T>* next;
-        Node<T>* prev;
     public:
         Node<T>* getNext(){
             return this->next;
-        }
-        Node<T>* getPrev(){
-            return this->prev;
         }
         T getContent(){
             return this->content;
@@ -31,6 +27,9 @@ class LinkedList {
     private:
         Node<T>* head;
     public:
+        LinkedList(){
+            this->head = NULL;
+        }
         void setHead(Node<T>* head){
             this->head = head;
         }
@@ -42,48 +41,34 @@ class LinkedList {
             for(int i = 0; i < index; i++){
                 temp = (*temp).getNext();
             }
-            cout<<(*temp).getContent()<<endl;
             return (*temp).getContent();
         }
         void add(T content){
-            // cout<<"setting head\n";
-            Node<T>* temp = this->getHead();
-            // cout<<"entering loop\n";
-            int i = 0;
-            while((*temp).getNext()){
-                // cout<<"loop: "<<i++<<endl;
+            Node<T>* temp;
+            if(this->head){
+                temp =  this->getHead();
+                while((*temp).getNext()){
+                    temp = (*temp).getNext();
+                }
+                temp->setNext(new Node<T>());
                 temp = (*temp).getNext();
             }
-            Node<T>* newtemp = new Node<T>();
-            (*temp).setNext(newtemp);
-            temp = (*temp).getNext();
+            else{
+                this->setHead(new Node<T>());
+                temp = this->getHead();
+            }
             (*temp).setContent(content);
-            cout<<(*temp).getContent()<<endl;
-        }
-        void deleteLL(){
-            Node<T>* temp = this->getHead();
-            if(!(*temp).getNext()){
-                delete temp;
-                return;
-            }
-            while((*temp).getNext()){
-                temp = (*temp).getNext();
-            }
-            delete temp;
-            deleteLL();
         }
 };
 
 
 int main() {
-    LinkedList<int> dll;
+    LinkedList<int> sll;
     int i;
     cin>>i;
-    cout<<"hello\n";
-    dll.add(i);
-    dll.add(++i);
-    // cout<<"dll get(0): "<<dll.get(0)<<endl;
-    dll.deleteLL();
-    delete &dll;
+    sll.add(i++);
+    sll.add(i);
+    cout<<"sll: "<<sll.get(0)<<endl;
+    cout<<"sll: "<<sll.get(1)<<endl;
     return 0;
 }
